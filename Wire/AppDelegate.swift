ed61bbbec46205130e29738e0248e19cf0b73aef
Will Tyler <will.tyler11@gmail.com>
2018-11-10 21:57:45 +0000

    Setup firebase

--- Wire/AppDelegate.swift
+++ Wire/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 
 @UIApplicationMain
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		FirebaseApp.configure()
+		
 		window = UIWindow(frame: UIScreen.main.bounds)
 
 		let tabBarController = TabBarController()
