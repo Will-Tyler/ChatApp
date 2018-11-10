e0fcbd3f73f11889a8d5f83332eafafc2f1416f4
Will Tyler <will.tyler11@gmail.com>
2018-11-10 20:28:49 +0000

    Give up on storyboard.... that was fast

--- Wire/AppDelegate.swift
+++ Wire/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
+		window = UIWindow(frame: UIScreen.main.bounds)
+
+		let homeViewController = UIViewController()
+
+		homeViewController.view.backgroundColor = UIColor.red
+		window!.rootViewController = homeViewController
+		window!.makeKeyAndVisible()
+		
 		return true
 	}
 	func applicationWillResignActive(_ application: UIApplication) {
